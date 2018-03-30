c361c6df183025f70a13a2a3167d5b6adc810331
rahul khanna <rahul.khanna@edfora.com>
2018-03-30 23:15:03 +0530

    prod add

--- config/docker_production.js
+++ config/docker_production.js
@@ -1,6 +1,5 @@
 const config = {
     "mode": "docker_production",
-    'process.env.PORT': 3021,
     "database":{
         "userName":"cmsv2test2",
         "password":"cmsv2test",
@@ -12,6 +11,36 @@ const config = {
         "port":9200,
         "hostname":"es.prod.mypat-internal.in"
  
+    },
+    "http": {
+        "protocol": "http",
+        "domain": "127.0.0.1",
+        "port": 3021
+    },
+    "appName": "cmsv2-dataService-es6",
+    "logger": {
+        "name": "cmsv2-backend-es6",
+        "streams": [
+            {
+                "level": "debug",
+                "stream": process.stdout
+            },
+            {
+                "level": "debug",
+                "path": "/var/log/cmsv2/cmsv2-dataService-es6-debug.log"
+            },
+            {
+                "level": "info",
+                "path": "/var/log/cmsv2/cmsv2-dataService-es6-info.log"
+            }
+        ]
+    },
+    "environmentVariableChecker": {
+        "isEnabled": false
+    },
+    "urlPrefix": "",
+    "authorization": {
+        "authorize": false
     }
  };
  module.exports = config;
