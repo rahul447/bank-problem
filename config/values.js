ec336809e75754cadea6a3e0b2869f51ded17a24
rahul khanna <rahul.khanna@edfora.com>
2018-05-16 20:17:33 +0530

    acl

--- config/values.js
+++ config/values.js
@@ -22,7 +22,7 @@ const values = {
     courseItemTypes: ['scheduledTests', 'sampleTests', 'syllabus', 'testGroup'],
     testTypes: ['full', 'part', 'practice', 'combined', 'sample', 'concept'],
     testStatuses: ['Live'],
-    validAccessTypes: ['CREATE', 'UPDATE', 'DELETE', 'SHARE', 'VIEW', 'PUBLISH', 'EDIT'],
-    featureTypes: ['COURSE', 'TEST', 'FORMULA', 'PACKAGE', 'TAG', 'LANGUAGE', 'GRADE', 'TESTTYPE', 'AUDIO', 'VIDEO', 'STUDYMATERIAL', 'QUESTION', 'CONTENT', 'TAGS', 'RULES']
+    validAccessTypes: ["CREATE", "UPDATE", "DELETE", "SHARE", "VIEW", "PUBLISH", "EDIT"],
+    featureTypes: ['COURSE', 'TEST', 'FORMULA', 'PACKAGE', 'TAG', 'LANGUAGE', 'GRADE', 'TESTTYPE', 'AUDIO', 'VIDEO', 'STUDYMATERIAL', 'QUESTION', 'CONTENT', 'TAGS', 'RULES', 'CLIENT']
 };
 module.exports = values;
