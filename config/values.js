7f532b2fa945fb7bd1c2e6a0528df61223569c85
rahul khanna <rahul.khanna@edfora.com>
2018-07-31 16:29:14 +0530

    chap support

--- config/values.js
+++ config/values.js
@@ -20,7 +20,7 @@ const values = {
         default: 'DRAFT'
     },
     courseItemTypes: ['scheduledTests', 'sampleTests', 'syllabus', 'testGroup', 'orgTests'],
-    testTypes: ['full', 'part', 'practice', 'combined', 'sample', 'concept'],
+    testTypes: ['full', 'part', 'practice', 'combined', 'sample', 'concept', 'chap'],
     testStatuses: ['Live'],
     validAccessTypes: ["CREATE", "UPDATE", "DELETE", "SHARE", "VIEW", "PUBLISH", "EDIT", "VALIDATE"],
     featureTypes: ['COURSE', 'TEST', 'FORMULA', 'PACKAGE', 'TAG', 'LANGUAGE', 'GRADE', 'TESTTYPE', 'AUDIO', 'VIDEO', 'STUDYMATERIAL', 'QUESTION', 'CONTENT', 'TAGS', 'RULES', 'CLIENT', 'CONTENTSETTING', 'COURSESYLLABUS', "USER", "RULE"],
