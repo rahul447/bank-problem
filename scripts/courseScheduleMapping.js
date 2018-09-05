18e934f536e2a871c03f66a94d8a9f1c42dd51cd
rahul khanna <rahul.khanna@edfora.com>
2018-09-05 13:56:06 +0530

    ..

---
+++ scripts/courseScheduleMapping.js
@@ -0,0 +1,38 @@
+print("CourseId, Name, scheduleIds, scheduleDates");
+var cursor = db.courses.aggregate([
+    {
+        "$unwind": "$courseItems"
+    },
+    {
+        "$match": {
+            "courseItems.itemType": "scheduledTests"
+        }
+    },
+    {
+        $lookup: {
+            from: "courseitems",
+            localField: "courseItems.id",
+            foreignField: "_id",
+            as: "courseItemData"
+        }
+    },
+    {
+        "$unwind": "$courseItemData"
+    },
+    {
+        "$unwind": "$courseItemData.details.schedule"
+    },
+    {
+        "$group": {
+            _id: "$_id",
+            names: { $addToSet: "$name" },
+            schIds: { $addToSet: "$courseItemData.details.schedule.scheduleID" },
+            scheduleDates: { $addToSet: "$courseItemData.details.schedule.scheduleDate" }
+        }
+    }
+]);
+while (cursor.hasNext()) {
+    jsonObject = cursor.next();
+
+    print(jsonObject._id+","+jsonObject.names[0]+","+jsonObject.schIds.join(", ")+","+jsonObject.scheduleDates.join(", "));
+}
